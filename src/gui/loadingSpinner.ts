6bf4d25c8cbd937d74c6ade4b2bd054cae600f7c
amami-harhid <haranaga.hidehiro@itimes.co.jp>
2026-06-25 13:31:40 +0900

    0.1.3 #01

--- src/gui/loadingSpinner.ts
+++ src/gui/loadingSpinner.ts
@@ -1,8 +1,11 @@
+import { Timer } from "./timer";
+
 export const loadingSpinner = ()=>{
  
-    window.addEventListener("load", function() {
+    window.addEventListener("load", async function() {
         const _overlay = document.querySelector('#loading-overlay') as HTMLDivElement;
         if(_overlay){
+            await Timer.wait(2000); // スピナーを最低2秒間は見せておく
             _overlay.style.display = "none";
         }
     });
